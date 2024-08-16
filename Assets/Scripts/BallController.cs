f3f40486b28fbc469ad82e959f823ed2e9e5613a
willy1989 <willy.winelover@gmail.com>
2024-08-16 21:46:34 +0200

    Added 4 direction movement controls (add force up, down, left, right)

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -10,19 +10,26 @@ public class BallController : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-            AddForceRight();
+        if(Input.GetKeyDown(KeyCode.W))
+            AddForce(Vector2.up);
+        if (Input.GetKeyDown(KeyCode.S))
+            AddForce(Vector2.down);
+        if (Input.GetKeyDown(KeyCode.A))
+            AddForce(Vector2.left);
+        if (Input.GetKeyDown(KeyCode.D))
+            AddForce(Vector2.right);
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
             ChangeScale(multiplier: 2f);
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
             ChangeScale(multiplier: 0.5f);
+        
     }
 
-    private void AddForceRight()
+    private void AddForce(Vector2 force)
     {
-        rigidBody.AddForce(Vector2.right * forceAmount, ForceMode2D.Impulse) ;
+        rigidBody.AddForce(force * forceAmount, ForceMode2D.Impulse) ;
     }
 
     private void ChangeScale(float multiplier)
