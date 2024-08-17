7878f6a91e360e024b77380ad8e1ac944a859153
willy1989 <willy.winelover@gmail.com>
2024-08-17 21:23:20 +0200

    Made level 2 and 3

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -8,8 +8,6 @@ public class PlayerMovementController : MonoBehaviour
 
     [SerializeField] private float forceAmount;
 
-    [SerializeField] private int uses = 0;
-
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
@@ -35,11 +33,7 @@ public class PlayerMovementController : MonoBehaviour
 
     private void AddForce(Vector2 force)
     {
-        if (uses == 0)
-            return;
-
         rigidBody.AddForce(force * forceAmount, ForceMode2D.Impulse) ;
-        uses--;
     }
 
     
