2edeb4c0d7e9282fbbbb7bc01417d13ad67664d1
Max <61809127+maxwellpark@users.noreply.github.com>
2024-08-20 15:21:41 +0100

    Tutorial updates (#19)
    * General info card
    
    * Show popup once
    
    * Product name
    
    * Reposition card

--- Assets/Scripts/UI/TutorialManager.cs
+++ Assets/Scripts/UI/TutorialManager.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class TutorialManager : MonoBehaviour
@@ -10,31 +11,51 @@ public class TutorialManager : MonoBehaviour
     [SerializeField] private GameObject popup;
     [SerializeField] private TMP_Text popupText;
     [SerializeField] private GameObject staticCard;
+    [SerializeField] private GameObject generalCard;
     [SerializeField] private GameObject background;
+
+    [SerializeField] private Config config;
 
     public event UnityAction OnTutorialOpened;
     public event UnityAction OnTutorialClosed;
 
-    // TODO: only show popup on first try of level?
     private void Start()
     {
         var staticText = staticCard.GetComponentInChildren<TMP_Text>();
         staticText.SetText(textContent);
         staticCard.SetActive(false);
+        generalCard.SetActive(false);
 
-        popup.SetActive(true);
-        popupText.SetText(textContent);
-        var button = popup.GetComponentInChildren<Button>();
-        button.onClick.AddListener(OnPopupClose);
+        var level = config.GetLevelBySceneName(SceneManager.GetActiveScene().name);
+        var key = $"{level.DisplayName}_PopupSeen";
+        var popupSeen = PlayerPrefs.GetInt(key, 0) == 1;
 
-        background.SetActive(true);
-        OnTutorialOpened?.Invoke();
+        if (!popupSeen)
+        {
+            popup.SetActive(true);
+            popupText.SetText(textContent);
+            var button = popup.GetComponentInChildren<Button>();
+            button.onClick.AddListener(OnPopupClose);
+
+            background.SetActive(true);
+            OnTutorialOpened?.Invoke();
+
+            PlayerPrefs.SetInt(key, 1);
+        }
+        else
+        {
+            popup.SetActive(false);
+            staticCard.SetActive(true);
+            generalCard.SetActive(true);
+            background.SetActive(false);
+        }
     }
 
     public void OnPopupClose()
     {
         popup.SetActive(false);
         staticCard.SetActive(true);
+        generalCard.SetActive(true);
         background.SetActive(false);
         OnTutorialClosed?.Invoke();
     }
